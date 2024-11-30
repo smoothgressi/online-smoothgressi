84d03130d2e35f0877268dbe7b0aa3c2c3b308d2
smoothgressi <Smoothgressi-web@outlook.com>
2024-11-30 17:50:22 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,32 @@
-// This is a simple service worker that caches essential assets
+const CACHE_NAME = 'smoothgressi-cache';
+const urlsToCache = [
+  '/online-smoothgressi/',
+  '/online-smoothgressi/index.html',
+  '/online-smoothgressi/styles.css',
+  '/online-smoothgressi/script.js',
+  '/online-smoothgressi/logo.png',
+  '/online-smoothgressi/manifest.json'
+];
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('smoothgressi-cache').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/styles.css',
-        '/script.js',
-        '/logo.png',
-        '/manifest.json'
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
